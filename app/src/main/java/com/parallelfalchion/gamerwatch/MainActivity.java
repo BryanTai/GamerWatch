bed14a8d32693e2bb18dc82446371fdcbbde1122
Bryan Tai <redhatpieman@gmail.com>
2016-09-24 10:44:42 -0700

    Added Layouts to single_game, created other resource xmls

--- app/src/main/java/com/parallelfalchion/gamerwatch/MainActivity.java
+++ app/src/main/java/com/parallelfalchion/gamerwatch/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //TODO This should be R.layout.landing_page.xml
+        setContentView(R.layout.single_game);
     }
 }
