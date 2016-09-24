0dbe583c174bb3e06f65f9f3dcc4267bdd2d3d5e
Bryan Tai <redhatpieman@gmail.com>
2016-09-24 15:33:07 -0700

    Added toolbar to landing page

--- app/src/main/java/com/parallelfalchion/gamerwatch/controllers/MainActivity.java
+++ app/src/main/java/com/parallelfalchion/gamerwatch/controllers/MainActivity.java
@@ -1,17 +1,30 @@
 package com.parallelfalchion.gamerwatch.controllers;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 import com.parallelfalchion.gamerwatch.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView featuredGames;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
 
-        //TODO This should be R.layout.landing_page.xml
-        setContentView(R.layout.single_game);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+
+        populateFeaturedGames();
+    }
+
+    private void populateFeaturedGames() {
+        featuredGames = (ListView) findViewById(R.id.featuredList);
+
     }
 }
